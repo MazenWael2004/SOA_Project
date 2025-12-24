ec3f49aed6114b904a628b5c62265020cb436c40
codinguniversal <125769338+codinguniversal@users.noreply.github.com>
2025-12-24 16:42:00 +0200

    removed commented out operations in order service

--- backend/services/orderService/app.py
+++ backend/services/orderService/app.py
@@ -49,70 +49,6 @@ def get_orders(order_id):
     if not order:
         return jsonify({"message": f"No order with id {order_id}", "status": 404}),404
     return jsonify(order.to_dict()), 200
-    # # initilize con and cursor
-    # con = None
-    # cursor = None
-    # try:
-    #     # setup
-    #     con = getCon()
-    #     cursor = con.cursor(dictionary = True)
-    #     sql_select_order = "SELECT * FROM orders JOIN order_items ON orders.order_id = order_items.order_id  WHERE orders.order_id = %s"
-    #     cursor.execute(sql_select_order, (order_id,))
-    #     rows = cursor.fetchall()
-    #     if not rows:
-    #         raise ValueError(f"No Order with ID: {order_id}")
-    #     customer_id = rows[0]["customer_id"]
-    #     order_date = rows[0]["order_date"]
-    #     total_amount = rows[0]["total_amount"]
-    #     products = []
-    #     for row in rows:
-    #         product_id = row["product_id"]
-    #         quantity = row["quantity"]
-    #         unit_price = row ["unit_price"]
-    #         products += [{"product_id" : product_id, "quantity": quantity, "unit_price" : unit_price }]
-
-    #     return jsonify({"customer_id":customer_id ,"order_date": order_date, "total_amount" : total_amount, "products": products}), 200
-    # except ValueError as ve:
-    #     return jsonify({"message" : str(ve) , "status": 404 , "status_text":"Not Found" }), 404
-    # finally:
-    #     if cursor: cursor.close()
-    #     if con: con.close()
-
-# def validate_order_input(data):
-#     if not data:
-#         raise ValueError("Empty or Invalid")
-#     if 'customer_id' not in data or 'products' not in data or 'total_amount' not in data:
-#         raise ValueError("Required Field Missing")
-
-# def validate_customer(cursor,customer_id):
-#     sql_Select_customer_id = "SELECT customer_id  FROM customers WHERE customer_id = %s "
-#     # get connection and cursor for transaction
-#     cursor.execute(sql_Select_customer_id, (customer_id,))
-#     row = cursor.fetchone()
-#     if not row:
-#         raise ValueError(f"No customer with ID {customer_id}")
-
-# def insert_order(cursor,customer_id, total_amount,return_order_id):
-
-#     sql_insert_orders = "INSERT INTO orders (customer_id, total_amount) values (%s, %s)"
-#     cursor.execute(sql_insert_orders, (customer_id, total_amount))
-#     if return_order_id:
-#         order_id = cursor.lastrowid
-#         return order_id
-
-# def get_product(cursor,product_id):
-
-#     sql_get_product = "SELECT *  FROM inventory WHERE  product_id = %s "
-#     cursor.execute(sql_get_product, (product_id,))
-#     product = cursor.fetchone()
-#     if not product:
-#         raise ValueError(f"Product with ID {product_id}, does not exist")
-
-#     return product
-# def insert_order_item(cursor,order_id , product_id , unit_price , quantity):
-
-#     sql_insert_order_items = "INSERT INTO order_items (order_id , product_id , unit_price , quantity) values(%s , %s , %s , %s)"
-#     cursor.execute(sql_insert_order_items ,(order_id , product_id , unit_price , quantity))
 
 @app.route("/api/orders/create", methods=["POST"])
 def create_order():
@@ -123,15 +59,15 @@ def create_order():
         return jsonify({"message": "the body was empty", "status": 400, "status_text": "Bad Request"}), 400
     # get values from the body
     customer_id = data.get("customer_id")
-    # total_amount = data['total_amount'] # Testing ALERT: Commented out for service testing
+    total_amount = data['total_amount'] # Testing ALERT: Commented out for service testing
     products = data.get('products')
     # validate expected values existence
     if not customer_id :
         return jsonify({"error": "Body is missing customer_id", "status": 400, "status_text": "Bad Request"}), 400
     
     # Testing ALERT: Commented out for service testing
-    # if not total_amount :
-    #     return jsonify({"error": "Body is missing total_amount", "status": 400, "status_text": "Bad Request"}), 400
+    if not total_amount :
+        return jsonify({"error": "Body is missing total_amount", "status": 400, "status_text": "Bad Request"}), 400
     
     if not products :
         return jsonify({"error": "Body is missing  products, or products list is empty", "status": 400, "status_text": "Bad Request"}), 400
@@ -141,8 +77,8 @@ def create_order():
         return jsonify({"error":"customer_id must be integer"}), 400
     
     # Testing ALERT: Commented out for service testing
-    # if not isinstance(total_amount, (int, float,Decimal)):
-    #     return jsonify({"error":"total_amount must be numeric"}), 400
+    if not isinstance(total_amount, (int, float,Decimal)):
+        return jsonify({"error":"total_amount must be numeric"}), 400
     
     if not isinstance(products, list):
         return jsonify({"error":"products must be a list"}), 400
